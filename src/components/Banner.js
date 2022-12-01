c564843a10ce7334dc473195704c7fbac8895266
Sakshi Thoutireddy <tsakshi01@gmail.com>
2022-11-30 19:16:10 -0800

    Built Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,37 +1,38 @@
-import React from "react";
-import styled from "styled-components";
-import { mediaQueries } from "../shared/config";
 
-const Container = styled.div`
+import React from 'react'; //import compenents from React
+import styled from 'styled-components'; //transforms styling Javascript to CSS
+import { mediaQueries } from '../shared/config';
+ 
+const StyleText = styled.div` //text is the styled component
+  font-family: Spectral, serif;
+  font-size: 22px;
+  background-color: #EDE8E5; //same as other elements background-color
+  color: black;
+  max-width: 80%;
+  height: fit-content;
+  position: relative;
+  ${mediaQueries.mobile}{
+    max-width: 90%;
+    font-size: 22px;
+  }
+`
+ 
+const StyleContainer = styled.div` //container is the styled component
   width: 100%;
-  height: 400px;
+  height: 350px;
   position: relative;
   background-color: #EDE8E5;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-`;
-
-const Text = styled.div`
-  position: absolute;
-  font-family: 'Spectral', serif;
-  font-size: 2em;
-  font-weight: 300;
-  max-width: 60%;
-  color: black;
-  ${mediaQueries.mobile} {
-    font-size: 20px;
-    max-width: 80%;
-  }
-`;
-
-export default function Banner(props) {
-    return (
-    <>
-        <Container>
-            <Text>{props.text}</Text>
-        </Container>
-    </>
-    );
-  }
+`
+ 
+ 
+export default function Banner(props){ //exporting function Banner called later in App.js
+ return(
+   <StyleContainer>
+     <StyleText>{props.text}</StyleText> 
+   </StyleContainer>
+ );
+}
